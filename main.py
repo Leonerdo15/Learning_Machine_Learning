1a8917f72b30fd99cf111013187dcebb5a3183a6
Leonerdo15 <leonardolage10@gmail.com>
2022-08-07 20:44:50 +0100

    MAchine learning otmizaçao

--- main.py
+++
@@ -1,4 +0,0 @@
-import panda as pd
-
-url = "https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv"
-pd.read_csv(url)
